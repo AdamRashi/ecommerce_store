ac95b323027ad993ce0a6399cf846b4a091b8059
Adam <adamrashi@mail.ru>
2022-08-24 09:53:35 +0300

    Basic functionality of store, including models of categories and products, views, html templates and some simple tests

---
+++ store/tests/test_models.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+
+from store.models import Category, Product, User
+
+
+class TestCategoriesModel(TestCase):
+
+    def setUp(self):
+        self.data1 = Category.objects.create(name="test category", slug='test-cat')
+
+    def test_category_model_entry(self):
+        """
+        Test Category model proper data insertion
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
+
+
+class TestProductsModel(TestCase):
+
+    def setUp(self):
+        Category.objects.create(name='test_cat', slug='test-cat')
+        User.objects.create(username='tester')
+        self.data1 = Product.objects.create(category_id=1,
+                                            created_by_id=1,
+                                            title='test_product',
+                                            slug='test-prod',
+                                            price=100)
+
+    def test_product_model_entry(self):
+        """
+        Test Product model proper data insertion and representation
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'test_product')
---
+++ store/tests/test_views.py
@@ -0,0 +1,8 @@
+# import unittest
+#
+# class MyTestCase(unittest.TestCase):
+#     def test_something(self):
+#         self.assertEqua(True, True)
+#
+# if __name__ == '__main__':
+#     unittest.main()
